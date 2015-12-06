eeb447e6190776252d7f7cde6e737d0069a38fd5
Rene <devproren@gmail.com>
2015-12-05 18:33:17 -0800

    added python algs to master

---
+++ algs/reconstruct.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, key, value, left = None, right = None):
+        self.key = key
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def reconstruct(pre, ino):
+    
+    pass
