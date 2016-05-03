b141abae884edd7bb3d292fac7d6cf638aa9371c
Rene <devproren@gmail.com>
2016-05-02 22:11:50 -0700

    implemted most of binarysearchtree

--- data_structures/tree.rb
+++ data_structures/tree.rb
@@ -38,13 +38,25 @@ class Tree
   def succ(key)
   end
 
-  # PriorityQueue methods
 
+  def del_min
+    delete(min)
+  end
+
+  def del_max
+    delete(max)
+  end
+
+  alias find_min min
+  alias find_max max
 end
 
 
 
-class BinaryTree
+# BinarySearchTree is a wrapper around Tree
+#
+# BinarySearchTree children are also BinarySearchTrees (too lazy to create a separate Node class)
+class BinarySearchTree
   def intialize(item:, left: nil, right: nil, parent: nil)
     @tree = Tree.new(item: item, children: [left, right], parent: parent)
   end
@@ -62,33 +74,101 @@ class BinaryTree
   end
 
   # Dictionary methods
+  # O(n), O(logn) if balanced
   def search(key)
-
+    _search(@tree, key)
   end
 
+
+  # `@tree` should never be nil
+  # O(n), O(logn) if balanced
   def insert(item)
-
+    _insert(@tree, item)
   end
 
-  def delete(item)
-
+  # passing in the node to delete, not the 'item'
+  def delete(node)
+    # Cases:
+    # 0) empty tree
+    # 1) leaf node
+    # 2) one child
+    # 3) two children
   end
 
+  # O(n), O(logn) if balanced
   def max
-
+    node = @tree
+    unless node.nil?
+      node = node.right_child
+    end
+    node
   end
 
+  # O(n), O(logn) if balanced
   def min
-
+    node = @tree
+    unless node.nil?
+      node = node.left_child
+    end
+    node
   end
 
+
+  # O(n), O(logn) if balanced
   def pred(key)
+    return nil if @tree.nil? or @tree.left_child.nil?
+    @tree.left_child.max
   end
 
+  # O(n), O(logn) if balanced
   def succ(key)
+    return nil if @tree.nil? or @tree.right_child.nil?
+    @tree.right_child.min
   end
 
-  # PriorityQueue methods
+
+  # O(n), O(logn) if balanced
+  def del_min
+    delete(min)
+  end
+
+  # O(n), O(logn) if balanced
+  def del_max
+    delete(max)
+  end
+
+  alias find_min min
+  alias find_max max
+
+
+  private
+  def _search(node, key)
+    return nil if node.nil?
+    return node if node.item == key
+
+    if key < node.item
+      _search(node.left_child, key)
+    else
+      _search(node.right_child, key)
+    end
+  end
+
+  # `node` should never be nil
+  def _insert(node, item)
+    if item < node.item
+      if node.left_child.nil?
+        node.left_child = BinarySearchTree.new item: item, parent: node
+      else
+        _insert(node.left, item)
+      end
+    else
+      if node.right_child.nil?
+        node.right_child = BinarySearchTree.new item: item, parent: node
+      else
+        _insert(node.right, item)
+      end
+    end
+  end
 
 end
 
