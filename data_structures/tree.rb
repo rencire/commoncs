f4968cc85f8ea1d7f718d742384ec36dcb6bddfd
Rene <devproren@gmail.com>
2016-05-03 22:05:24 -0700

    added a Node class, started implementing `delete()`. Need to start writing tests, or continue cowboy coding

--- data_structures/tree.rb
+++ data_structures/tree.rb
@@ -1,7 +1,6 @@
 
 
-class Tree
-
+class TreeNode
   attr_accessor :item, :children, :parent
 
   def intialize(item:, children: [], parent: nil)
@@ -9,7 +8,9 @@ class Tree
     @children = children
     @parent = parent
   end
+end
 
+class Tree
 
   # Dictionary methods
   def search(key)
@@ -40,11 +41,9 @@ class Tree
 
 
   def del_min
-    delete(min)
   end
 
   def del_max
-    delete(max)
   end
 
   alias find_min min
@@ -56,84 +55,196 @@ end
 # BinarySearchTree is a wrapper around Tree
 #
 # BinarySearchTree children are also BinarySearchTrees (too lazy to create a separate Node class)
-class BinarySearchTree
-  def intialize(item:, left: nil, right: nil, parent: nil)
-    @tree = Tree.new(item: item, children: [left, right], parent: parent)
+
+
+class BinaryNode
+  def initialize(left: nil, right: nil, parent: nil)
+    @node = nodeNode.new(item: item, children: [left, right], parent: parent)
   end
 
   def left_child
-    @tree.children[0]
+    @node.children[0]
+  end
+
+  def left_child=(c)
+    @node.children[0] = c
   end
 
   def right_child
-    @tree.children[1]
+    @node.children[1]
+  end
+
+  def right_child=(c)
+    @node.children[1] = c
   end
 
   def item
-    @tree.item
+    @node.item
   end
+
+  def parent
+    @node.parent
+  end
+
+  def is_leaf?
+    left_child.nil? and right_child.nil?
+  end
+end
+
+class BinarySearchTree
+
+  def intialize
+    @root = nil
+  end
+
 
   # Dictionary methods
   # O(n), O(logn) if balanced
   def search(key)
-    _search(@tree, key)
+    _search(@root, key) do |node, k|
+      return -1 if node.item < k
+      return 0 if node.item == k
+      return 1 if node.item > k
+    end
   end
 
-
-  # `@tree` should never be nil
   # O(n), O(logn) if balanced
   def insert(item)
-    _insert(@tree, item)
+    if @root.nil?
+      @root = BinaryNode.new item: item
+      return
+    end
+
+    _insert(@root, item)
   end
 
-  # passing in the node to delete, not the 'item'
+  # NOTE:
+  # passing in the node object to delete, not the 'item'
+  #
+  # O(n) since our implementation checks if node exists in tree first.
+  # O(1) otherwise if we don't need to check
+  #
+  # TODO
+  # consider refactoring out the cases. getting cumbersome to read the code.
   def delete(node)
     # Cases:
-    # 0) empty tree
+    # 0) root is nil
+    return if @root.nil?
+
+    # check if node object exists in tree
+    found = _search(@root, node) do |n, key|
+      return -1 if n.item < key.item
+      return 0 if n == key
+      return 1 if n.item > key.item
+    end
+
+    if found.nil?
+      raise Exception("Cannot delete node; #{node} is not in tree")
+    end
+
+
     # 1) leaf node
+    if node.is_leaf? and node == @root
+      @root = nil
+      return
+    end
+
+    if node.is_leaf? and node == node.parent.left_child
+      node.parent.left_child = nil
+      return
+    end
+
+    if node.is_leaf? and node == node.parent.right_child
+      node.parent.right_child = nil
+      return
+    end
+
+
     # 2) one child
+    # TODO
+    # Refactor this. Code is not very 'DRY'
+    if !node.left_child.nil? and node.right_child.nil?
+
+      if node == @root
+        @root = node.left_child
+        return
+      end
+
+      if node.parent.left_child == node
+        node.parent.left_child = node.left_child
+      else
+        node.parent.right_child = node.left_child
+      end
+
+      # unlink `node` from rest of tree
+      node.parent = nil
+      node.left_child = nil
+      node.right_child = nil
+
+      return
+    end
+
+    if node.left_child.nil? and !node.right_child.nil?
+      if node == @root
+        @root = node.right_child
+        return
+      end
+
+      if node.parent.left_child == node
+        node.parent.left_child = node.right_child
+      else
+        node.parent.right_child = node.right_child
+      end
+
+      # unlink `node` from rest of tree
+      node.parent = nil
+      node.left_child = nil
+      node.right_child = nil
+
+      return
+    end
+
+
     # 3) two children
+    # this is going to be a hairy solution...
   end
 
   # O(n), O(logn) if balanced
   def max
-    node = @tree
-    unless node.nil?
-      node = node.right_child
-    end
-    node
+    return if @root.nil?
+    _max(@root)
   end
+
 
   # O(n), O(logn) if balanced
   def min
-    node = @tree
-    unless node.nil?
-      node = node.left_child
-    end
-    node
+    return if @root.nil?
+    _min(@root)
   end
 
 
   # O(n), O(logn) if balanced
   def pred(key)
-    return nil if @tree.nil? or @tree.left_child.nil?
-    @tree.left_child.max
+    return nil if @root.nil? or @root.left_child.nil?
+    _max(@root.left_child)
   end
 
   # O(n), O(logn) if balanced
   def succ(key)
-    return nil if @tree.nil? or @tree.right_child.nil?
-    @tree.right_child.min
+    return if @root.nil? or @root.right_child.nil?
+    _min(@root.right_child)
   end
 
 
   # O(n), O(logn) if balanced
   def del_min
+    return if @root.nil?
     delete(min)
   end
 
   # O(n), O(logn) if balanced
   def del_max
+    return if @root.nil?
     delete(max)
   end
 
@@ -142,14 +253,17 @@ class BinarySearchTree
 
 
   private
+  # Takes in a comparitor to determine comparison
   def _search(node, key)
     return nil if node.nil?
-    return node if node.item == key
+    return node if yield(node, key) == 0
 
-    if key < node.item
+    if yield(node, key) == -1
       _search(node.left_child, key)
-    else
+    elsif yield(node, key) == 1
       _search(node.right_child, key)
+    else
+      raise Exception "Comparitor #{comp} did not return 0, -1, nor 1"
     end
   end
 
@@ -157,19 +271,32 @@ class BinarySearchTree
   def _insert(node, item)
     if item < node.item
       if node.left_child.nil?
-        node.left_child = BinarySearchTree.new item: item, parent: node
+        node.left_child = BinaryNode.new item: item, parent: node
       else
         _insert(node.left, item)
       end
     else
       if node.right_child.nil?
-        node.right_child = BinarySearchTree.new item: item, parent: node
+        node.right_child = BinaryNode.new item: item, parent: node
       else
         _insert(node.right, item)
       end
     end
   end
 
+  def _max(node)
+    unless node.right_child.nil?
+      node = node.right_child
+    end
+    node
+  end
+
+  def _min(node)
+    unless node.nil?
+      node = node.left_child
+    end
+    node
+  end
 end
 
 
