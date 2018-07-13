82024060239a821f8439252e19cb0f3c2fecc385
Eric Ren <546296+rencire@users.noreply.github.com>
2018-07-13 16:14:45 -0700

    Create heap.js

---
+++ data_structures/heap.js
@@ -0,0 +1,22 @@
+class Heap {
+  // Array of elements
+  constructor(elements) {
+    if (Array.isArray(elements)) {
+      this.heap = elements
+    } else {
+      this.heap = []
+    }
+  }
+
+  push() {
+
+  }
+
+  pop() {
+
+  }
+
+
+}
+
+export default Heap
